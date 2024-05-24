05afb5dd187f761ee574fb4b4f93772137dbc381
Vivien <127337820+shadoine@users.noreply.github.com>
2024-05-24 19:23:15 +0200

    Update custom.js

--- custom.js
+++ custom.js
@@ -4,27 +4,3 @@
       return false;
     });
   });
- const emailBox = document.querySelector(".js-email p");
-
-emailBox.addEventListener("click", () => {
-  const selection = window.getSelection();
-  const range = document.createRange();
-  range.selectNodeContents(emailBox);
-  selection.removeAllRanges();
-  selection.addRange(range);
-  try {
-    document.execCommand("copy");
-    selection.removeAllRanges();
-
-    const original = emailBox.textContent;
-    emailBox.textContent = "Copied!";
-    emailBox.classList.add("success");
-
-    setTimeout(() => {
-      emailBox.textContent = original;
-      emailBox.classList.remove("success");
-    }, 1200);
-  } catch (e) {
-    console.log("Failed to copy");
-  }
-});
